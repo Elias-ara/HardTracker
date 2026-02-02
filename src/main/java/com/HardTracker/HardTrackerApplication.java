ef1436b29ddf29b8d64a35c0c352ae7c688bbb92
Elias-ara <eliasmirandaaraujo8@gmail.com>
2026-02-02 20:00:39 -0300

    First commit: backend structure with Spring Boot

---
+++ src/main/java/com/HardTracker/HardTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.HardTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HardTracker/HardTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HardTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
